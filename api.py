3a1dc77754e7e6589b8ed53e8abc15cf712eeceb
hecnavar <hmnm0007@gmail.com>
2025-08-03 21:04:49 -0600

    Feat: Different functions are created to create items and subitems

--- api.py
+++ api.py
@@ -1,6 +1,29 @@
 import requests
 import json
 
+def send_query(api_url, api_key, query):
+    """
+    Helper function to send the query to the API
+        
+    Args: 
+        api_url: Url API.
+        api_key: API key for monday.
+        query: Query text string
+    Return:
+        response: The server response in dictionary format.
+    """
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": api_key
+    }
+    data = {"query": query}
+
+    print("Sending Data ......")
+    response = requests.post(url=api_url, headers=headers, json=data)
+    
+    return response.json()
+
+
 def create_item(api_url, api_key, id_key, item_name, column_replace):
     """
     Funtion for create a new item on monday
@@ -12,7 +35,7 @@ def create_item(api_url, api_key, id_key, item_name, column_replace):
         id_key: Value fot board Id
         item_name : Give a new name to the new element
     Return:
-
+        send_query: Send the query with the parameters that were sent to you
     """
 
     column_replace_json = json.dumps(column_replace)
@@ -29,14 +52,35 @@ def create_item(api_url, api_key, id_key, item_name, column_replace):
     }}
     """
 
-    headers = {
-    "Content-Type": "application/json",
-    "Authorization": api_key
-    }
+    return send_query(api_url, api_key, new_query)
 
-    new_data = {"query": new_query}
 
-    print("Sending Data ......")
-    response = requests.post(url=api_url, headers=headers, json=new_data)
+def create_subitem(api_url, api_key, parent_item_id, item_name, column_replace):
+    """
+    Funtion for create a new item on monday
 
-    return response.json()
+    Args: 
+        api_url: Url API.
+        api_key: API key for monday
+        column_replace: New data
+        id_key: Value fot board Id
+        item_name : Give a new name to the new element
+    Return:
+        send_query: Send the query with the parameters that were sent to you
+    """
+
+    column_replace_json = json.dumps(column_replace)
+
+    new_query = f"""
+    mutation {{
+        create_subitem (
+            parent_item_id: {parent_item_id},
+            item_name: "{item_name}",
+            column_values: {json.dumps(column_replace_json)}
+        ){{
+            id
+        }}
+    }}
+    """
+
+    return send_query(api_url, api_key, new_query)
